43070bf74a0839daea993e74e4366634a5ccd8aa
Alexandre Gélinas <gelinas.alexandre.2001@gmail.com>
2021-02-20 01:00:50 -0500

    Ajout du TP1

---
+++ TP1/comparison.h
@@ -0,0 +1,7 @@
+
+
+// This is the function to call when comparing the newton approxima6on to the real root.
+float comparison(float initial_value, float root);
+// This function returns the positive value of comparison
+float comparison_absolutie(float initial_value, float root);
+
---
+++ TP1/readme.txt
@@ -0,0 +1,3 @@
+Équipière 1 : Daria Ioana Danciu #2081744
+Équipier 2 :  Alexandre Gélinas #2083465
+
